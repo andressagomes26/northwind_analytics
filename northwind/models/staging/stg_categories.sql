8c6febfa433aa8467e77b8234e35066986b7e498
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-08 19:15:01 -0300

    Definição da stg_categories

---
+++ northwind/models/staging/stg_categories.sql
@@ -0,0 +1,12 @@
+with 
+    dados_categorias as (
+        select 
+            category_id
+            , category_name
+            , description
+            , picture
+        from {{ source('sources_data', 'categories') }}
+    )
+
+select *
+from dados_categorias
