45218d17f4e3c1e978a37f0b1444b0d2fb304863
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-08 20:26:26 -0300

    Definição da stg_employees

---
+++ northwind/models/staging/stg_employees.sql
@@ -0,0 +1,26 @@
+with 
+    dados_funcionarios as (
+        select 
+            employee_id
+            , last_name
+            , first_name
+            , title
+            , title_of_courtesy
+            , birth_date
+            , hire_date
+            , address
+            , city
+            , region
+            , postal_code
+            , country
+            , home_phone
+            , extension
+            , photo
+            , notes
+            , reports_to
+            , photo_path
+        from {{ source('sources_data', 'employees') }}
+    )
+
+select *
+from dados_funcionarios
